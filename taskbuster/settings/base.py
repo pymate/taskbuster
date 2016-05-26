82b2d0bd8c68222dbebfcd263b3a50363edc4ba5
Py Mate <pymate@gmail.com>
2016-05-26 19:24:46 -0400

    Settings, static files and templates

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -63,8 +63,7 @@ ROOT_URLCONF = 'taskbuster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
